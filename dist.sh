2fd549fb37571012944e851439b182d995bea633
Alexander Suter <alexander.suter@axonivy.com>
2023-07-09 08:04:41 +0200

    Add README.md

--- dist.sh
+++ dist.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm handball.zip
-zip -r handball.zip . -x ".git/*" -x "*.yaml" -x "*.sh" -x ".gitignore"
+zip -r handball.zip . -x ".git/*" -x "*.yaml" -x "*.sh" -x ".gitignore" -x "README.md"
